fabbcb100d90dad87630958d96a8b74a53abc8d0
carolysg <carolinagoshima@gmail.com>
2024-11-19 11:28:59 -0300

    fix extract data

--- dags/breweries_dag.py
+++ dags/breweries_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import requests
 import pandas as pd
+import numpy as np
 import psycopg2
 from psycopg2.extras import execute_values
 from dotenv import load_dotenv
@@ -39,10 +40,21 @@ def create_dirs():
 def extract_brewery_data():
     create_dirs()
     try:
-        response = requests.get("https://api.openbrewerydb.org/breweries")
-        response.raise_for_status()
-        data = response.json()
-        df = pd.DataFrame(data)
+        response_total = requests.get("https://api.openbrewerydb.org/breweries/meta")
+        total = int(response_total.json()['total'])
+        per_page = 200
+        num_page = int(np.ceil(total / per_page))
+        df = pd.DataFrame()
+        for page in range(num_page):
+            params = {
+                "page": page + 1,
+                "per_page": per_page
+            }
+            response = requests.get("https://api.openbrewerydb.org/breweries", params=params)
+            response.raise_for_status()
+            data = response.json()
+            df_sample = pd.DataFrame(data)
+            df = pd.concat([df, df_sample], ignore_index=True)
         df.to_json('./data/bronze/breweries_raw.json', orient='records')
         logging.info("Brewery data extracted successfully.")
     except requests.exceptions.RequestException as e:
@@ -53,6 +65,8 @@ def transform_data():
     try:
         df = pd.read_json('./data/bronze/breweries_raw.json')
         df.dropna(axis=1, how='all', inplace=True)
+        df['longitude'] = df['longitude'].astype(float)
+        df['latitude'] = df['latitude'].astype(float)
         df_final = df.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
         if df_final['state_province'].equals(df_final['state']):
             df_final.drop('state', axis=1, inplace=True)
